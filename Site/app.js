095e0e758830299a4ff8dd6955ba329d60a6e1bc
green13 <vd_tereshchenko@WS11452.bbgroup.local>
2015-09-24 17:01:51 +0300

    Added ejs template engine

--- Site/app.js
+++ Site/app.js
@@ -1,24 +1,62 @@
+var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var compression = require('compression');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(compression());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.raw());
 app.use(cookieParser());
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/api', function (req, res) {
     res.send('API is running');
 });
+app.use('/', require('./routes/index'));
+app.use('/api/webm', require('./routes/api/webm'));
 
-app.use('/api/webm', require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 
 //// route to handle all angular requests
