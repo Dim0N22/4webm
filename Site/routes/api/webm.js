8e9b9902590f42563beb3ba9e900ca37794c1762
green13 <vd_tereshchenko@WS11452.bbgroup.local>
2015-09-24 17:23:30 +0300

    Added views

--- Site/routes/api/webm.js
+++ Site/routes/api/webm.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
     res.send('webm');
 });
+
 
 module.exports = router;
--- Site/routes/index.js
+++ Site/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+
+router.get('/:id([0-9]+)', function (req, res, next) {
+    res.render('view', {title: 'View ' + req.params.id});
+});
+
+
+router.get('/edit/:id([0-9]+)?', function (req, res, next) {
+    res.render('edit', {title: 'Edit ' + req.params.id});
+});
+
 
 module.exports = router;
