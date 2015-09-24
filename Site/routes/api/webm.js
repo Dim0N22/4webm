e335706f28ec6fee486e7bf5fc467b4827affad2
green13 <vdtereshchenko@gmail.com>
2015-09-24 22:46:59 +0300

    Added navigation arrows on edit page and added some api routes

--- Site/routes/api/webm.js
+++ Site/routes/api/webm.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.send('webm');
 });
 
 
-// TODO add tag to video
+/**
+ * Sets webm's property (tags as an example)
+ *
+ * @params property, action, value
+ */
+router.put('/:id([0-9]+)', function(req, res) {
+    if (req.body.property === "tags" && (req.body.action === 'add' || req.body.action === 'remove') && req.body.value){
+        // TODO db.webm.updateTags(req.body.action, req.body.value)
+        res.status(200).end();
+    } else {
+        res.status(400).end();
+    }
+});
+
+
+router.put('/:id([0-9]+)/next', function(req, res) {
+    res.send('webm');
+});
+
+
+router.put('/:id([0-9]+)/prev', function(req, res) {
+    res.send('webm');
+});
+
 
 module.exports = router;
--- Site/routes/index.js
+++ Site/routes/index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.render('index', {title: 'Express'});
 });
 
 
-router.get('/:id([0-9]+)', function (req, res, next) {
+router.get('/:id([0-9]+)', function (req, res) {
     res.render('view', {title: 'webm ' + req.params.id});
 });
 
 
-router.get('/edit/:id([0-9]+)', function (req, res, next) {
+router.get('/edit/:id([0-9]+)', function (req, res) {
     var tags = [
         {name: 'tag1', enable: true},
         {name: 'tag2'}];
