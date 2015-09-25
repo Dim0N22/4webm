0356b4bfdece9a635a0967b4720460c9e96d8cc5
green13 <vdtereshchenko@gmail.com>
2015-09-25 14:45:20 +0300

    Main view

--- Site/routes/index.js
+++ Site/routes/index.js
@@ -11,7 +11,34 @@ router.get('/', function (req, res) {
 
 
 router.get('/:id([0-9]+)', function (req, res) {
-    res.render('view', {title: 'webm ' + req.params.id});
+    var id = Number(req.params.id);
+
+    db.webms.findOne({seqid: id}, function (err, webm) {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+            return;
+        }
+
+        if (!webm) {
+            res.redirect('/');
+            return;
+        }
+
+        var tags = [];
+        for (var i = 0; i < webm.tags.length; i++) {
+            tags.push({name: webm.tags[i]})
+        }
+
+        res.render('view', {
+            title: '4webm #' + id,
+            id: id,
+            videoSrc: url.resolve(config.videoServer, String(webm.file_info.path).slice(2)),
+            tags: tags,
+            prevHref: id > 1 ? '/' + (id - 1) : '/',
+            nextHref: '/' + (id + 1)
+        });
+    });
 });
 
 
@@ -48,7 +75,7 @@ router.get('/edit/:id([0-9]+)', function (req, res) {
             }
 
             res.render('edit', {
-                title: 'Edit ' + id,
+                title: '4webm edit #' + id,
                 id: id,
                 videoSrc: url.resolve(config.videoServer, String(webm.file_info.path).slice(2)),
                 tags: tags,
