d986e8e6fad33efb3870dcbaa71c5321ace6bef6
green13 <vdtereshchenko@gmail.com>
2015-09-26 18:42:18 +0300

    Passing tags array to server through cookies

--- Site/public/index.js
+++ Site/public/index.js
@@ -1,7 +1,12 @@
 function clickTag(tag) {
     var action;
+    var tagName = tag.id;
+    var tags = Cookies.getJSON('tags') || [];
+
     if ($(tag).hasClass("btn-default")) {
         // add tag
+
+        tags.push(tagName);
 
         action = 'add';
         $(tag).removeClass("btn-default");
@@ -9,18 +14,37 @@ function clickTag(tag) {
     } else {
         // remove tag
 
+        var ind = tags.indexOf(tagName);
+        if (ind > -1) {
+            tags.splice(ind, 1);
+        }
+
         action = 'remove';
         $(tag).addClass("btn-default");
         $(tag).removeClass("btn-success");
     }
 
-    //$.ajax({
-    //    url: '/api/webm/' + webmId,
-    //    type: 'PUT',
-    //    data: {
-    //        property: 'tags',
-    //        action: action,
-    //        value: tag.innerHTML
-    //    }
-    //});
+    Cookies.set('tags', tags);
+
+    // TODO refresh videos
 }
+
+function addMoarWebms() {
+    //
+}
+
+function changeMainPage() {
+    //
+}
+
+$(function () {
+    var tags = Cookies.getJSON('tags') || [];
+
+    for (var i in tags) {
+        if (tags.hasOwnProperty(i)) {
+            var el = document.getElementById(tags[i]);
+            el.classList.add("btn-success");
+            el.classList.remove("btn-default");
+        }
+    }
+});
