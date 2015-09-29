52c65e919473e0a569d443c1545924f166887825
green13 <vdtereshchenko@gmail.com>
2015-09-29 13:55:13 +0300

    Added navigating through touch events (swipe)

--- Site/public/view.js
+++ Site/public/view.js
@@ -156,3 +156,48 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('likeABoss').classList.add('enabled-boss');
     }
 });
+
+
+// ------------------------------------------------------------------------------
+// swipe
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            /* left swipe */
+            document.getElementById('next').click();
+        } else {
+            /* right swipe */
+            document.getElementById('prev').click();
+        }
+    } else {
+        if (yDiff > 0) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+}
