1dbdcf44aef25d70505a47d2f1e1f27e29e84d0a
green13 <vdtereshchenko@gmail.com>
2015-10-13 11:55:39 +0300

    Added navigation with swipe and keyboard on page doubles.view

--- Site/public/doubles/view.js
+++ Site/public/doubles/view.js
@@ -15,3 +15,67 @@ function double(isDouble, id, el) {
         }
     });
 }
+
+
+// ------------------------------------------------------------------------------
+// hotkeys
+Mousetrap.bind('right', next);
+Mousetrap.bind('left', prev);
+
+function next() {
+    document.getElementById('next').click();
+}
+
+function prev() {
+    document.getElementById('prev').click();
+}
+
+
+// ------------------------------------------------------------------------------
+// swipe
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchend', handleTouchEnd, false);
+
+var xDown = null;
+var yDown = null;
+var threshold = 150; //required min distance traveled to be considered swipe
+var restraint = 100; // maximum distance allowed at the same time in perpendicular direction
+var allowedTime = 400; // maximum time allowed to travel that distance
+var startTime;
+var elapsedTime;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+    startTime = new Date().getTime(); // record time when finger first makes contact with surface
+}
+
+function handleTouchEnd(evt) {
+    console.log("2", evt);
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.changedTouches[0].clientX;
+    var yUp = evt.changedTouches[0].clientY;
+
+    elapsedTime = new Date().getTime() - startTime;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (elapsedTime <= allowedTime) {
+        if (Math.abs(xDiff) >= threshold && Math.abs(yDiff) <= restraint) {
+            if (xDiff > 0) {
+                /* left swipe */
+                document.getElementById('next').click();
+            } else {
+                /* right swipe */
+                document.getElementById('prev').click();
+            }
+        }
+    }
+
+    xDown = null;
+    yDown = null;
+}
